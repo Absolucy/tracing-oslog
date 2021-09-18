35363c6e72f55cfa7968ae5830d415fd94b8205c
Lucy <lucy@absolucy.moe>
2021-09-18 16:25:46 -0400

    🩹 Fix the thingy not compiling

--- build.rs
+++ build.rs
@@ -21,7 +21,7 @@ fn main() {
 		.expect("Unable to generate bindings");
 
 	// Write the bindings to the $OUT_DIR/bindings.rs file.
-	let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+	let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
 	bindings
 		.write_to_file(out_path)
 		.expect("Couldn't write bindings!");
