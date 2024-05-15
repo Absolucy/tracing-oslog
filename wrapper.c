9c6ccece29d1235625079b3dc319bc6d50c288cb
therealbnut <635596+therealbnut@users.noreply.github.com>
2024-05-15 12:19:26 +1000

    Make OsLogger implement Default via OS_LOG_DEFAULT

--- wrapper.c
+++ wrapper.c
@@ -3,3 +3,6 @@
 void wrapped_os_log_with_type(os_log_t log, os_log_type_t type, const char* message) {
     os_log_with_type(log, type, "%{public}s", message);
 }
+os_log_t wrapped_os_log_default(void) {
+    return OS_LOG_DEFAULT;
+}
--- wrapper.h
+++ wrapper.h
@@ -1,3 +1,5 @@
 #include <os/log.h>
 #include <os/activity.h>
-void wrapped_os_log_with_type(os_log_t log, os_log_type_t type, const char* message);
+
+void wrapped_os_log_with_type(os_log_t log, os_log_type_t type, const char *message);
+os_log_t wrapped_os_log_default(void);
