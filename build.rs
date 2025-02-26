99725402fd943cc973678671d28f12c9297ef82f
Mads Marquart <mads@marquart.dk>
2025-02-26 02:26:47 +0100

    Fix building on docs.rs

--- build.rs
+++ build.rs
@@ -3,6 +3,13 @@ use std::env;
 fn main() {
 	println!("cargo:rerun-if-changed=build.rs"); // Enable change-tracking
 
+	if std::env::var("DOCS_RS").is_ok() {
+		// docs.rs doesn't have the required Xcode header files, so skip
+		// building in this case. The documentation will still work since it
+		// doesn't need to link.
+		return;
+	}
+
 	if env::var("CARGO_CFG_TARGET_VENDOR").expect("failed to get target vendor") != "apple" {
 		println!(
 			"cargo:warning=tracing-oslog is only available for Apple platforms, it will not log \
